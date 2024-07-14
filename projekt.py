2e359db8d2467c8e2e74018b2db67efca63265b2
daria <dariuszakasprzyk@gmail.com>
2024-07-14 16:44:18 +0200

    dodano json

--- projekt.py
+++ projekt.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 def parsuj():
     parser = argparse.ArgumentParser(description='Konwerter Danych')
@@ -6,7 +7,16 @@ def parsuj():
     parser.add_argument('plik_wyjsciowy', type=str, help='Ścieżka do pliku wyjściowego')
     return parser.parse_args()
 
+def wczytaj_json(sciezka_pliku):
+    with open(sciezka_pliku, 'r') jako plik:
+        try:
+            dane = json.load(plik)
+            return dane
+        except json.JSONDecodeError as e:
+            print(f"Błąd odczytu pliku JSON: {e}")
+            return None
+
 if __name__ == "__main__":
     argumenty = parsuj()
     print(f"Plik wejściowy: {argumenty.plik_wejsciowy}")
-    print(f"Plik wyjściowy: {argumenty.plik_wyjsciowy}")
+    print(f"Plik
